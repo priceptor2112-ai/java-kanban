4649f1539fe405658e360f24bf13782d0677f5b5
priceptor2112-ai <priceptor2112@gmail.com>
2025-11-16 15:56:30 +0300

    fix: remove package declaration

--- src/taskmanager/service/TaskManager.java
+++ src/taskmanager/service/TaskManager.java
@@ -3,39 +3,39 @@ import java.util.List;
 public interface TaskManager {
     List<Task> getAllTasks();
 
-    List<Epic> getAllEpics();
-
-    List<Subtask> getAllSubtasks();
-
-    void deleteAllTasks();
-
-    void deleteAllEpics();
-
-    void deleteAllSubtasks();
-
     Task getTask(int id);
-
-    Epic getEpic(int id);
-
-    Subtask getSubtask(int id);
 
     void addTask(Task task);
 
-    void addEpic(Epic epic);
-
-    void addSubtask(Subtask subtask);
-
     void updateTask(Task task);
-
-    void updateEpic(Epic epic);
-
-    void updateSubtask(Subtask subtask);
 
     void deleteTask(int id);
 
+    void deleteAllTasks();
+
+    List<Epic> getAllEpics();
+
+    Epic getEpic(int id);
+
+    void addEpic(Epic epic);
+
+    void updateEpic(Epic epic);
+
     void deleteEpic(int id);
 
+    void deleteAllEpics();
+
+    List<Subtask> getAllSubtasks();
+
+    Subtask getSubtask(int id);
+
+    void addSubtask(Subtask subtask);
+
+    void updateSubtask(Subtask subtask);
+
     void deleteSubtask(int id);
+
+    void deleteAllSubtasks();
 
     List<Subtask> getEpicSubtasks(int epicId);
 
